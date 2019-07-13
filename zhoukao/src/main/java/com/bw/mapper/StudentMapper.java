78ed56c683b31577506832ede945b960904e8800
zangwanqing <1935996900@qq.com>
2019-07-13 10:27:06 +0800

    删除了zhoukao

--- zhoukao/src/main/java/com/bw/mapper/StudentMapper.java
+++
@@ -1,9 +0,0 @@
-package com.bw.mapper;
-
-import java.util.List;
-
-import com.bw.entity.Student;
-
-public interface StudentMapper {
-	List<Student> getlist (Student student);
-}
--- zhoukao/src/main/java/com/bw/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.bw.service;
-
-import java.util.List;
-
-import com.bw.entity.Student;
-
-public interface StudentService {
-	List<Student> getlist (Student student);
-}
